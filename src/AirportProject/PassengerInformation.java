284378aa2557b12aa708cd399d495ea8dd187e4e
Anthony <amcmullen9291@gmail.com>
2021-11-16 16:58:08 -0500

    safety commit

--- src/AirportProject/PassengerInformation.java
+++ src/AirportProject/PassengerInformation.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class PassengerInformation  extends JFrame {
     static PassengerInformation register;
@@ -27,7 +35,7 @@ public class PassengerInformation  extends JFrame {
         panelFields1.setLayout(new GridLayout(15,1));
         panelFields1.setSize(175,350);
         JPanel panelFields2 = new JPanel();
-        panelFields2.setLayout(new GridLayout(9,1));
+        panelFields2.setLayout(new GridLayout(3,1));
 //        panelFields2.setSize(175,350);
 //setLocation was here
         panel.setSize(550,350);
@@ -72,9 +80,23 @@ public class PassengerInformation  extends JFrame {
         panel.add(customerInfo1);
 
         // start panel 2 here?
+        JLabel dateLabel = new JLabel("Today's Date:");
         JPanel customerInfo2 = new JPanel();
         customerInfo2.setBackground(Color.GREEN);
-
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        LocalTime theDate = LocalTime.now();
+        dateLabel.setHorizontalAlignment(JTextField.CENTER);
+        JTextField date = new JTextField(String.valueOf(theDate));
+        date.setHorizontalAlignment(JTextField.CENTER);
+        ZoneId zonedId = ZoneId.of( "America/Montreal" );
+        LocalDate today2 = LocalDate.now( zonedId );
+        JLabel date2 = new JLabel("<html><c>"+String.valueOf(today2)+"</c></html>");
+        date2.setHorizontalAlignment(JTextField.CENTER);
+        panelFields2.add(date2);
+        panelFields2.add(dateLabel);
+        panelFields2.add(date);
+//        panelFields2.add(date2);
         customerInfo2.add(panelFields2);
 
         //panel 3 below here
@@ -92,4 +114,5 @@ public class PassengerInformation  extends JFrame {
         System.out.println("Welcome to our Airport!");
         passengerInformation();
     }
+
 }
