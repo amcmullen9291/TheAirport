3e15a4b72b65fd9e55da615f0dac2414e31a9d2e
Anthony <amcmullen9291@gmail.com>
2021-11-16 17:06:18 -0500

    add date and time

--- src/AirportProject/PassengerInformation.java
+++ src/AirportProject/PassengerInformation.java
@@ -7,8 +7,10 @@ import java.awt.*;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -86,8 +88,12 @@ public class PassengerInformation  extends JFrame {
         Calendar today = Calendar.getInstance();
         today.set(Calendar.HOUR_OF_DAY, 0);
         LocalTime theDate = LocalTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd"+", "+ "HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String theTime = dtf.format(now);
+
         dateLabel.setHorizontalAlignment(JTextField.CENTER);
-        JTextField date = new JTextField(String.valueOf(theDate));
+        JTextField date = new JTextField(String.valueOf(theTime));
         date.setHorizontalAlignment(JTextField.CENTER);
         ZoneId zonedId = ZoneId.of( "America/Montreal" );
         LocalDate today2 = LocalDate.now( zonedId );
@@ -96,6 +102,7 @@ public class PassengerInformation  extends JFrame {
         panelFields2.add(date2);
         panelFields2.add(dateLabel);
         panelFields2.add(date);
+        panelFields2.setBackground(Color.GRAY);
 //        panelFields2.add(date2);
         customerInfo2.add(panelFields2);
 
