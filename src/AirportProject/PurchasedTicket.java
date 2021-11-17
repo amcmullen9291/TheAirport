755b490b8e922fefee23d1992fc9748edb90045d
Anthony <amcmullen9291@gmail.com>
2021-11-16 23:22:08 -0500

    suggestions for boardingPass random string generation

--- src/AirportProject/PurchasedTicket.java
+++ src/AirportProject/PurchasedTicket.java
@@ -1,4 +1,30 @@
 package AirportProject;
 
+import java.util.Random;
+
 public class PurchasedTicket {
+
+    static String ticketNumberString ="";
+    static double discount = 0.00;
+
+    public static void purchasedTicket(){
+
+    }
+
+
+    public void getTicketString() { // just a suggestion for randomly generated boardingPass Number
+        String ticketNumber = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 9) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * ticketNumber.length());
+            salt.append(ticketNumber.charAt(index));
+        }
+        ticketNumberString = salt.toString();
+    }
+
+    public double discountedTicketPrice(int birthYear){
+        // if birthYear is less than ________ or larger than_________
+        return discount;
+    }
 }
